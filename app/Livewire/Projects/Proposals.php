a2c444991de7b5b2369af890e5d0e3f53c080cd0
Willian Moreno <willian_moreno@outlook.com>
2024-10-09 19:09:42 -0300

    feat: Carregando mais propostas com paginação

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,11 +3,27 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Proposals extends Component
 {
     public Project $project;
+
+    public int $visibleQuantity = 10;
+
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->project->proposals()
+            ->orderByDesc('hours')
+            ->simplePaginate($this->visibleQuantity);
+    }
+
+    public function loadMore()
+    {
+        $this->visibleQuantity += 10;
+    }
 
     public function render()
     {
