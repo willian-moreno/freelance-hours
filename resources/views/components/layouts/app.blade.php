2cb031d0c51471cc8cd74012cd7ee3e6a2dbba61
Willian Moreno <willian_moreno@outlook.com>
2024-10-09 11:01:55 -0300

    feat: Adicionando novos componentes e assets

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,10 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>{{ $title ?? config('app.name') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-    {{ $slot }}
+<body class="bg-[#070715] flex flex-col min-h-full text-[#C3C3D1]">
+    <x-ui.navbar />
+    <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 w-full py-[40px]">
+        {{ $slot }}
+    </main>
 </body>
 
 </html>
---
+++ resources/views/components/ui/card.blade.php
@@ -0,0 +1,4 @@
+<div
+    {{ $attributes->class(['bg-[#10101E] divide-[#1E1E2C] divide-y p-[40px] rounded-[10px] border-[#1E1E2C] border']) }}>
+    {{ $slot }}
+</div>
