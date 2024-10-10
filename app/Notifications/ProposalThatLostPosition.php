7299e802fbc485e0b453e2375ac5b7e06691bf2f
Willian Moreno <willian_moreno@outlook.com>
2024-10-10 16:18:27 -0300

    feat: Adicionando ao serviço notificação estado de fila

--- app/Notifications/ProposalThatLostPosition.php
+++ app/Notifications/ProposalThatLostPosition.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ProposalThatLostPosition extends Notification
+class ProposalThatLostPosition extends Notification implements ShouldQueue
 {
     use Queueable;
 
